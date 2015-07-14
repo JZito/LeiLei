dfd2333942bf41dfad7c59b619b1b1bf21d768b6
Jeff <oakisland@gmail.com>
2015-07-14 09:48:09 -0400

    tracing

--- Assets/Scripts/AnimateArrow.cs
+++ Assets/Scripts/AnimateArrow.cs
@@ -15,22 +15,28 @@ public class AnimateArrow : MonoBehaviour {
 	public TraceTrigger pointB;
 	public bool curve;
 	public int compensation;
+	public int moreCompensation;
+	private int ogCompensation;
 	private List<GameObject> arrows = new List<GameObject>();
 	//public float fader;
 
 	// Use this for initialization
 	void Start () {
+		ogCompensation = compensation;
+		//set original compensation so animation resets, determine itween path params
 		iTween.MoveTo(arrow, iTween.Hash("path",iTweenPath.GetPath(pathName),"lookTime", 0.2,"moveToPath", false,"easetype",  "linear", "looptype", "loop", "lookahead", 0.0001, "onComplete", "ClearArrows", "oncompletetarget", this.gameObject,"time",animationTime));
 		//p = iTweenPath.GetPath("pathroll");
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		//
 		Vector3 moveDirection = arrow.transform.position; 
 		if (moveDirection != Vector3.zero) 
 		{
 
 			if (curve){
+				//if it's a curve we gotta make the arrow turn
 				float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
 				arrow.transform.rotation = Quaternion.AngleAxis(angle - compensation, Vector3.forward);
 			}
@@ -40,18 +46,25 @@ public class AnimateArrow : MonoBehaviour {
 		timeCounter += Time.deltaTime;
 		if (timeCounter >= trailTime)
 		{
+			// create a new arrow prefab in a fading trail
 			timeCounter = 0;
 			GameObject f = Instantiate(fader, new Vector3(arrow.transform.position.x, arrow.transform.position.y, arrow.transform.position.z + 1), arrow.transform.rotation) as GameObject;
 			SpriteRenderer faderSprite = f.GetComponent<SpriteRenderer>();
 			faderSprite.sprite = arrow.GetComponent<SpriteRenderer>().sprite;
 			arrows.Add(f);
 			f.GetComponent<FadeMaterials>().FadeOut();
+			//further adjust for the curve if necessary based on each step
+			compensation = compensation + moreCompensation ;
 		}
 	}
 
 	public void ClearArrows () {
+		// get rid of the arrows and reset arrow curve compensation
 		for (int j = 0; j < arrows.Count; j++) {
 			GameObject.Destroy(arrows[j]);
+			pointA.touched = false;
+			pointB.touched = false;
+			compensation = ogCompensation;
 		}
 	}
 
---
+++ Assets/Scripts/TraceStepHandler.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TraceStepHandler : MonoBehaviour {
+
+	public AnimateArrow[] steps;
+	public bool complete;
+	public List<AnimateArrow> completedSteps = new List<AnimateArrow>();
+	private int counter = 0;
+	private AnimateArrow currentStep;
+	private GameObject finalStep;
+	//public GameObject letter;
+
+	// Use this for initialization
+	void Start () {
+		counter = 0;
+		complete = false;
+//		finalStep = steps [steps.Length];
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (counter >= steps.Length) {
+			complete = true;
+
+		} 
+		else if (counter < steps.Length) {
+			currentStep = steps [counter];
+			if (Input.GetMouseButtonUp (0)) {
+				currentStep.pointA.GetComponent<SpriteRenderer> ().color = currentStep.pointA.untouchedColor;
+			}
+			else if (Input.GetMouseButton (0) || Input.GetMouseButtonDown (0)) {
+				//final step = steps[steps.length];
+				if (currentStep.pointA.touched) { 
+					//if (breaker.touched ) { 
+					// interrupt touch and start over;
+					//}
+					if (currentStep.pointB.touched) {
+						currentStep.gameObject.SetActive (false);
+						currentStep.ClearArrows();
+						counter++;
+						steps [counter].gameObject.SetActive (true);
+					}
+				}
+			}
+		}
+	}
+}
